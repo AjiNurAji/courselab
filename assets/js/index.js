243e90ffc3a1f6d2532a2790a79ace1813cb960b
AjiNurAji <ajinuraji090306@gmail.com>
2023-11-29 17:44:14 +0700

    contact page and hero section

--- assets/js/index.js
+++ assets/js/index.js
@@ -55,20 +55,3 @@ particlesJS("particles-js", {
   },
   retina_detect: true,
 });
-
-const logo = document.getElementById("logo");
-const navbar = document.getElementById("navbar");
-
-const scrollHandle = () => {
-  const scrolY = window.scrollY;
-
-  if (scrolY > 0) {
-    logo.src = "./assets/images/logo.svg";
-    navbar.classList.add("active");
-  } else {
-    logo.src = "./assets/images/logo-white.svg";
-    navbar.classList.remove("active");
-  }
-};
-
-window.addEventListener("scroll", scrollHandle);
